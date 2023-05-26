20189b4fff264f71ecf0c66552ba934114662860
Caio <whoiscaio@protonmail.com>
2023-05-26 08:48:34 -0300

    feat: authentication server added

---
+++ src/auth.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import authRouter from './routes/authRouter';
+
+const PORT = 3001;
+
+const auth = express();
+
+auth.use(express.json());
+auth.use(authRouter);
+
+auth.listen(PORT, () => {
+  console.log(`Auth started at http://localhost:${PORT}`);
+})
---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,16 @@
+type User = {
+  username: string,
+  password: string
+}
+
+const users: User[] = [];
+
+class AuthController {
+  login() {}
+
+  signup() {}
+
+  getAccessToken() {}
+}
+
+export default new AuthController();
